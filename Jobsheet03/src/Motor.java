3b66cdb915f299f64e6ea754284a8545d6cadc52
NeroClaudia <itzfaraways10@gmail.com>
2026-09-07 19:53:00 +0700

    Change validation

--- Jobsheet03/src/Motor.java
+++ Jobsheet03/src/Motor.java
@@ -25,7 +25,11 @@ public class Motor {
     }
 
     public void setKecepatan(int kecepatan) {
-        if (!this.isMesinOn && kecepatan > 0) {
+        if (kecepatan < 0) {
+            System.out.println("Kecepatan tidak boleh negatif");
+        } else if (kecepatan > 100) {
+            System.out.println("Kecepatan maksimal adalah 100");
+        } else if (!this.isMesinOn && kecepatan < 0) {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
         } else {
             this.kecepatan = kecepatan;
