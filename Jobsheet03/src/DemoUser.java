bbdbb5b2b412d3f9f32964088651f4d7c15c8298
NeroClaudia <itzfaraways10@gmail.com>
2026-09-08 11:54:52 +0700

    Add User class main to test

---
+++ Jobsheet03/src/DemoUser.java
@@ -0,0 +1,7 @@
+public class DemoUser {
+    public static void main(String[] args) {
+        User user1 = new User("Annisa.Nadya", "Annisa Nadya", "annisa.nadya@gmail.com");
+
+        user1.cetakInfo();
+    }
+}
