092a4e335eb45aa00a9601d0bf7a431574fec924
NeroClaudia <itzfaraways10@gmail.com>
2026-09-22 13:28:30 +0700

    Adding one more object

--- Quiz2/src/MainBengkel.java
+++ Quiz2/src/MainBengkel.java
@@ -5,17 +5,23 @@ public class MainBengkel {
 
         CustomerBengkel cust1 = new CustomerBengkel("Andi Susanto", "081234567890");
 
-        Vehicle car = new Vehicle("B 1234 XYZ", "Toyota", "Avanza", "Car");
+        Vehicle car1 = new Vehicle("B 1234 XYZ", "Toyota", "Avanza", "Car");
+        Vehicle car2 = new Vehicle("AG 1567 GAS", "Lamborghini", "Aventador", "Car");
         Vehicle motorcycle = new Vehicle("B 2312 GYH", "Honda", "Vario", "Motorcycle");
+        Vehicle motorcycle2 = new Vehicle("N 2871 GAH", "Honda", "Beat", "Motorcycle");
 
         cust1.addVehicle(motorcycle);
-        cust1.addVehicle(car);
+        cust1.addVehicle(car1);
+        cust1.addVehicle(car2);
+        cust1.addVehicle(motorcycle2);
 
         Service oilChangeCar = new Service("Ganti oli mesin", 300000);
         Service tuneUpMotor = new Service("Tune up lengkap", 100000);
 
         emp1.processService(cust1.getVehicles().get(0), oilChangeCar);
         emp1.processService(cust1.getVehicles().get(1), tuneUpMotor);
+        emp1.processService(cust1.getVehicles().get(2), oilChangeCar);
+        emp1.processService(cust1.getVehicles().get(3), tuneUpMotor);
 
     }
 }
